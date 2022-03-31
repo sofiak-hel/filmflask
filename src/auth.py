47a3dd51920f0104910107b8659e240cef90edab
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-03-31 22:11:36 +0300

    Add a register page

--- src/auth.py
+++ src/auth.py
@@ -3,7 +3,7 @@ from argon2 import PasswordHasher
 from typing import Optional
 from datetime import time
 
-from db import login, find_session_and_user, delete_session
+from db import login, find_session_and_user, delete_session, create_user
 
 
 class User:
@@ -31,6 +31,10 @@ class User:
     @staticmethod
     def logout(session_id: int):
         delete_session(session_id)
+
+    @staticmethod
+    def register(handle: str, nickname: str, password: str):
+        return create_user(handle, nickname, password)
 
     def __str__(self):
         return "%s (@%s)" % (self.nickname, self.handle)
--- src/db.py
+++ src/db.py
@@ -12,48 +12,74 @@ with open("src/sql/shorts.json") as f:
 db = SQLAlchemy()
 
 
-def create_user(handle: str, nickname: str, password: str):
-    password_hash = hasher.hash(password)
-    db.session.execute(sql["create_user"], {
-        "handle": handle,
-        "nickname": nickname,
-        "password_hash": password_hash
-    })
+def create_user(handle: str, nickname: str, password: str) -> bool:
+    try:
+        password_hash = hasher.hash(password)
+        db.session.execute(sql["create_user"], {
+            "handle": handle,
+            "nickname": nickname,
+            "password_hash": password_hash
+        })
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 
 def login(handle: str, password: str) -> Optional[dict]:
-    password_hash = hasher.hash(password)
-    result = db.session.execute(sql["find_user_handle"], {"handle": handle})
-    user = result.fetchone()
-    if user is None:
+    try:
+        password_hash = hasher.hash(password)
+        result = db.session.execute(
+            sql["find_user_handle"], {"handle": handle})
+        user = result.fetchone()
+        if user is None:
+            return None
+        elif not hasher.verify(user["password_hash"], password):
+            return None
+        else:
+            return create_session(user["user_id"])
+    except Exception as e:
+        print(e)
         return None
-    elif not hasher.verify(user["password_hash"], password):
-        return None
-    else:
-        return create_session(user["user_id"])
 
 
 def create_session(user_id: str) -> Optional[dict]:
-    result = db.session.execute(sql["create_session"], {"user_id": user_id})
-    db.session.commit()
-    session = result.fetchone()
-    if session is not None:
-        return find_session_and_user(session["session_id"])
-    return None
+    try:
+        result = db.session.execute(
+            sql["create_session"], {"user_id": user_id})
+        db.session.commit()
+        session = result.fetchone()
+        if session is not None:
+            return find_session_and_user(session["session_id"])
+        return None
+    except Exception as e:
+        print(e)
+        return None
 
 
 def find_session_and_user(session_id: int) -> Optional[dict]:
-    data = {"session_id": session_id}
-    db.session.execute(sql["clear_old_sessions"])
-    db.session.commit()
-    result = db.session.execute(sql["find_session_and_user"], data).fetchone()
-    if result is not None:
+    try:
+        data = {"session_id": session_id}
+        db.session.execute(sql["clear_old_sessions"])
+        db.session.commit()
+        result = db.session.execute(
+            sql["find_session_and_user"], data).fetchone()
+        if result is None:
+            return None
         db.session.execute(sql["refresh_session"], data)
         db.session.commit()
         return result
-    return None
+    except Exception as e:
+        print(e)
+        return None
 
 
-def delete_session(session_id):
-    db.session.execute(sql["delete_session"], {"session_id": session_id})
-    db.session.commit()
+def delete_session(session_id) -> bool:
+    try:
+        db.session.execute(sql["delete_session"], {"session_id": session_id})
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
